54443422b8f75280c02f14046a1150f8bda6b021
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2024-10-14 14:44:16 +0000

    Curso em C#

---
+++ For/Program.cs
@@ -0,0 +1,30 @@
+// string [] names = {"Gustavo", "Laura", "Adriana", "Rogerio"};
+// for (int i = names.Length -1 ; i >= 0; i --){
+//     Console.WriteLine(names[i]);;
+// }
+
+// string[] names = { "Gustavo", "Laura", "Adriana", "Rogerio" };
+// for (int i = 0; i < names.Length; i++)
+// {
+//     if (names[i] == "Gustavo")
+//     {
+//         names[i] = "Teste";
+//     }
+// }
+// foreach (var name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// for (int i = 0; i < 101; i++)
+// {
+//     if ((i % 3 == 0) && (i % 5 == 0))
+//         Console.WriteLine($"{i} - FizzBuzz");
+//      else if (i % 3 == 0)
+//         Console.WriteLine($"{i} - Fizz");
+//     else if (i % 5 == 0)
+//         Console.WriteLine($"{i} - Buzz");
+//     else
+//         Console.WriteLine($"{i}");
+
+// }
